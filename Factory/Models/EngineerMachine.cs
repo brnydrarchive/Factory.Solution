bb1f9ce56c0cd0c00b1aaf9788cdb11dff1bd748
Brian Yoder <brianyoder@gmail.com>
2023-03-23 11:16:36 -0700

    create Engineer class

---
+++ Factory/Models/EngineerMachine.cs
@@ -0,0 +1,12 @@
+namespace Factory.Models
+{
+  public class EngineerMachine
+  {
+    public int EngineerMachineId { get; set; }
+    public Engineer Engineer { get; set; }
+    public int EngineerId { get; set; }
+    public Machine Machine { get; set; }
+    public int MachineId { get; set; }
+
+  }
+}
