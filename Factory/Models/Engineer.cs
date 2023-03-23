5ec7c42d5c67f3b5d946e86174e8426ede80d230
Brian Yoder <brianyoder@gmail.com>
2023-03-23 11:53:34 -0700

    add datavalidation

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "You must enter a name for an engineer. ")]
+    [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "The name must contain only alphabetic characters.")]
     public string Name { get; set; }
     public List<EngineerMachine> JoinEntities { get; set; }
   }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "You must enter a machine type to create an instance of machine.")]
     public string Type { get; set; }
     public List <EngineerMachine> JoinEntities { get; set; }
   }
